46e1ceb5f712b719362887bea692755b606c013b
David Michaels <dmichaels@gmail.com>
2025-04-08 16:01:02 -0400

    etc

--- PixelBabel/PixelMap.swift
+++ PixelBabel/PixelMap.swift
@@ -26,20 +26,12 @@ class PixelMap {
         self._pixelsHeight = height
         self._scale = scale
         self._pixels = [UInt8](repeating: 0, count: self._pixelsWidth * self._pixelsHeight * ScreenDepth)
-        print("CREATE-PIXELS: [\(self._pixelsWidth * self._pixelsHeight * ScreenDepth)]")
-        // xyzzy
-        self._pixels.withUnsafeBufferPointer { buffer in
-            let address = buffer.baseAddress
-            let raw = UInt(bitPattern: address)
-            let hex = String(raw, radix: 16, uppercase: true)
-            print("CREATE-PIXELS-INIT-ADDRESS: [\(hex)]")
-        }
-        // xyzzy
+        print("PixelMap.init.CREATE-PIXELS: [\(self._pixelsWidth * self._pixelsHeight * ScreenDepth)]")
         self._producer = producer
         if (producer) {
             self._pixelsList = []
             self._pixelsListAccessQueue = DispatchQueue(label: "PixelBabel.PixelMap.ACCESS" /*, qos: .background */)
-            self._pixelsListReplenishQueue = DispatchQueue(label: "PixelBabel.PixelMap.REPLENISH", qos: .background)
+            self._pixelsListReplenishQueue = DispatchQueue(label: "PixelBabel.PixelMap.REPLENISH" /*, qos: .background */)
             self._replenish()
         }
     }
@@ -54,12 +46,12 @@ class PixelMap {
 
     public var scale: Int {
         get { return self._scale }
-        set { self._scale = newValue }
+        set { self._scale = newValue ; self.invalidate() }
     }
 
     public var mode: ColorMode {
         get { return self._mode }
-        set { self._mode = newValue }
+        set { self._mode = newValue ; self.invalidate() }
     }
 
     public var data: [UInt8] {
@@ -73,90 +65,25 @@ class PixelMap {
         if (self._producer) {
             var pixels: [UInt8]? = nil
             self._pixelsListAccessQueue!.sync {
-                print("RANDOMIZE: IN-SYNC-BLOCK: [\(self._pixelsList!.count)]")
+                print("PixelMap.randomize.IN-SYNC-BLOCK: [\(self._pixelsList!.count)]")
                 // This block of code effectively synchronizes
                 // access to pixelsList between producer and consumer.
                 if !self._pixelsList!.isEmpty {
                     pixels = self._pixelsList!.removeFirst()
-                    print("RANDOMIZE: IN-SYNC-BLOCK: [\(self._pixelsList!.count)] GOT-PIXELS [\(pixels != nil ? 1 : 0)]")
-                    done = true
                 }
                 else {
-                    print("PixelMap.randomize.initial")
+                    print("PixelMap.randomize.PIXEL-LIST-EMPTY")
                 }
             }
-            print("RANDOMIZE: AFTER-SYNC-BLOCK: [\(pixels != nil ? 1 : 0)] [\(done)]")
             if (pixels != nil) {
-                print("RANDOMIZE: USE-CACHED PIXELS")
+                print("PixelMap.randomize.USE-CACHED-PIXELS")
                 self._pixels = pixels!
+                done = true
             }
             self._replenish()
         }
         if (!done) {
-            print("RANDOMIZE: USE-EXISTING PIXELS")
-            PixelMap._randomize(&self._pixels, self._pixelsWidth, self._pixelsHeight,
-                                width: self.width, height: self.height, scale: self.scale, mode: self.mode)
-        }
-    }
-
-
-    public func obsolete_randomize()
-    {
-        var done: Bool = false
-        print("xyzzy.a")
-        if (self._producer) {
-            print("xyzzy.b")
-            self._pixelsListAccessQueue!.async {
-                print("FOO: [\(self._pixelsList!.count)]")
-                print("xyzzy.c")
-                // This block of code effectively synchronizes
-                // access to pixelsList between producer and consumer.
-                if !self._pixelsList!.isEmpty {
-                    print("xyzzy.d")
-                    print("PixelMap.randomize.grab-pre-computed-pixels-todo: [\(self._pixelsList!.count)]")
-                    var pixels: [UInt8]? = self._pixelsList!.removeFirst()
-                    print("PixelMap.randomize.grab-pre-computed-pixels-done: [\(self._pixelsList!.count)]")
-                    // xyzzy
-                    pixels!.withUnsafeBufferPointer { buffer in
-                        let address = buffer.baseAddress
-                        let raw = UInt(bitPattern: address)
-                        let hex = String(raw, radix: 16, uppercase: true)
-                        print("GRAB-PIXELS-ADDRESS: [\(hex)]")
-                    }
-                    // xyzzy
-                    self._pixels = pixels!
-                    pixels = nil
-                    done = true
-                    // pixels = nil
-                    // print("PixelMap.randomize.c")
-                    // print("PixelMap.randomize.d")
-                }
-                else {
-                    print("xyzzy.e")
-                    print("PixelMap.randomize.initial")
-                }
-                print("xyzzy.f")
-            }
-            print("xyzzy.g")
-            self._replenish()
-            print("xyzzy.h")
-        }
-        print("xyzzy.i")
-        if (!done) {
-            print("randomize.xyzzy")
-            // let pixelMap: PixelMap = PixelMap(self._pixelsWidth, self._pixelsHeight,
-            //                                   scale: self._scale, mode: self._mode, producer: false)
-            // PixelMap.obsolete_randomize(pixelMap)
-            // self._pixels = pixelMap._pixels
-            // PixelMap.obsolete_randomize(self)
-            // xyzzy
-            self._pixels.withUnsafeBufferPointer { buffer in
-                let address = buffer.baseAddress
-                let raw = UInt(bitPattern: address)
-                let hex = String(raw, radix: 16, uppercase: true)
-                print("CALL-RANDOMIZE-FROM-NOT-DONE-PIXELS-ADDRESS: [\(hex)]")
-            }
-            // xyzzy
+            print("PixelMap.randomize.USE-EXISTING-PIXELS")
             PixelMap._randomize(&self._pixels, self._pixelsWidth, self._pixelsHeight,
                                 width: self.width, height: self.height, scale: self.scale, mode: self.mode)
         }
@@ -199,14 +126,6 @@ class PixelMap {
     static func _randomize(_ pixels: inout [UInt8], _ pixelsWidth: Int, _ pixelsHeight: Int,
                            width: Int, height: Int, scale: Int, mode: ColorMode)
     {
-        // xyzzy
-        pixels.withUnsafeBufferPointer { buffer in
-            let address = buffer.baseAddress
-            let raw = UInt(bitPattern: address)
-            let hex = String(raw, radix: 16, uppercase: true)
-            print("RANDOMIZE-PIXELS-ADDRESS: [\(hex)]")
-        }
-        // xyzzy
         for y in 0..<height {
             for x in 0..<width {
                 if (mode == ColorMode.monochrome) {
@@ -241,16 +160,7 @@ class PixelMap {
                 let iy = y * scale + dy
                 let i = (iy * pixelsWidth + ix) * ScreenDepth
                 if ((ix < pixelsWidth) && (i < pixels.count)) {
-                    //xyzzy
-                    // print("XX: [\(i)] [\(pixels.count)]")
-                    if i >= 0 && i < pixels.count {
-                        pixels[i] = red
-                    }
-                    else {
-                        print("ERROR: index \(index) out of bounds; pixels.count = \(pixels.count)")
-                    }
-                    //xyzzy
-                    // pixels[i] = red
+                    pixels[i] = red
                     pixels[i + 1] = green
                     pixels[i + 2] = blue
                     pixels[i + 3] = transparency
@@ -260,60 +170,33 @@ class PixelMap {
     }
 
     private func _replenish() {
-        // DispatchQueue.global(qos: .background).async {
         self._pixelsListReplenishQueue!.async {
-            print("_replenish.start")
+            print("_replenish.START")
             // This block of code runs OFF of the main thread (i.e. in the background);
             // and no more than one of these will ever be running at a time. 
             var additionalPixelsProbableCount: Int = 0
-            // self._pixelsListAccessQueue!.async {
+            self._pixelsListAccessQueue!.sync {
                 // This block of code is effectively to synchronize
                 // access to pixelsList between (this) producer and consumer.
                 additionalPixelsProbableCount = self._pixelsListMax - self._pixelsList!.count
-                print("_replenish.probable-count: [\(self._pixelsListMax)] - [\(self._pixelsList!.count)] = [\(additionalPixelsProbableCount)]")
-            // }
-            print("_replenish.probable-count-after-async: [\(additionalPixelsProbableCount)]")
+                print("_replenish.ADDITIONAL-PROBABLE-COUNT: [\(self._pixelsListMax)] - [\(self._pixelsList!.count)] = [\(additionalPixelsProbableCount)]")
+            }
             if (additionalPixelsProbableCount > 0) {
-                print("_replenish.probable-count-yes: [\(additionalPixelsProbableCount)]")
-                var additionalPixelsProbableList: [[UInt8]] = []
                 for i in 0..<additionalPixelsProbableCount {
-                    print("_replenish.probable-count-loop: [\(i)] in [\(additionalPixelsProbableCount)]")
-                    if (true) {
-                        var pixels = [UInt8](repeating: 0, count: self._pixelsWidth * self._pixelsHeight * ScreenDepth)
-                        print("CREATE-PIXELS-PRODUCER: [\(self._pixelsWidth * self._pixelsHeight * ScreenDepth)]")
-                        // xyzzy
-                        pixels.withUnsafeBufferPointer { buffer in
-                            let address = buffer.baseAddress
-                            let raw = UInt(bitPattern: address)
-                            let hex = String(raw, radix: 16, uppercase: true)
-                            print("CREATE-PIXELS-PRODUCER-ADDRESS: [\(hex)]")
+                    var pixels: [UInt8] = [UInt8](repeating: 0, count: self._pixelsWidth * self._pixelsHeight * ScreenDepth)
+                    PixelMap._randomize(&pixels, self._pixelsWidth, self._pixelsHeight,
+                                        width: self.width, height: self.height, scale: self.scale, mode: self.mode)
+                    print("replenish.CREATED-PIXELS: [\(self._pixelsWidth * self._pixelsHeight * ScreenDepth)]")
+                    self._pixelsListAccessQueue!.sync {
+                        if (self._pixelsList!.count < self._pixelsListMax) {
+                            print("replenish.QUEUEING-CACHED-PIXELS: [\(self._pixelsList!.count)]")
+                            self._pixelsList!.append(pixels)
+                            print("replenish.QUEUED-CACHED-PIXELS: [\(self._pixelsList!.count)]")
                         }
-                        // xyzzy
-                        PixelMap._randomize(&pixels, self._pixelsWidth, self._pixelsHeight,
-                                            width: self.width, height: self.height, scale: self.scale, mode: self.mode)
-                        additionalPixelsProbableList.append(pixels)
-                    }
-                    else {
-                        let pixelMap: PixelMap = PixelMap(self._pixelsWidth, self._pixelsHeight,
-                                                          scale: self._scale, mode: self._mode, producer: false)
-                        pixelMap.randomize()
-                        additionalPixelsProbableList.append(pixelMap._pixels)
-
-                    }
-                }
-                self._pixelsListAccessQueue!.sync {
-                    let additionalPixelsActualCount: Int = self._pixelsListMax - self._pixelsList!.count
-                    print("_replenish.actual-count: [\(self._pixelsListMax)] - [\(self._pixelsList!.count)] = [\(additionalPixelsActualCount)]")
-                    if (additionalPixelsActualCount > 0) {
-                        // This block of code is effectively to synchronize
-                        // access to pixelsList between (this) producer and consumer.
-                        print("replenishPixelsListAsync.additional-actual-count: [\(additionalPixelsActualCount)] to [\(self._pixelsList!.count)]")
-                        self._pixelsList!.append(contentsOf: additionalPixelsProbableList.prefix(additionalPixelsActualCount))
-                        print("replenishPixelsListAsync.additional-append-done: [\(additionalPixelsActualCount)] to [\(self._pixelsList!.count)]")
                     }
                 }
             }
-            print("_replenish.end")
+            print("_replenish.END")
         }
     }
 }
