8eb76e473e56cefdf1ae02ef2bdc8f43021ba422
David Michaels <dmichaels@gmail.com>
2025-04-12 00:16:44 -0400

    etc

--- PixelBabel/Pixel.swift
+++ PixelBabel/Pixel.swift
@@ -5,7 +5,10 @@ import SwiftUI
 //
 struct Pixel: Equatable {
 
-    var _value: UInt32
+    var _red: UInt8
+    var _green: UInt8
+    var _blue: UInt8
+    var _alpha: UInt8 = 255
 
     public static let black: Pixel = Pixel(0, 0, 0)
     public static let white: Pixel = Pixel(255, 255, 255)
@@ -13,23 +16,57 @@ struct Pixel: Equatable {
     public static let light: Pixel = Pixel(200, 200, 200)
 
     public var red: UInt8 {
-        get { UInt8((self._value >> 24) & 0xFF) }
+        get { self._red }
     }
 
     public var green: UInt8 {
-        get { UInt8((self._value >> 16) & 0xFF) }
+        get { self._green }
     }
 
     public var blue: UInt8 {
-        get { UInt8((self._value >> 8) & 0xFF) }
+        get { self._blue }
     }
 
-    public var transparency: UInt8 {
-        get { UInt8(self._value & 0xFF) }
+    public var alpha: UInt8 {
+        get { self._alpha }
     }
 
-    init(_ red: UInt8, _ green: UInt8, _ blue: UInt8, transparency: UInt8 = 255) {
-        self._value = (UInt32(red) << 24) | (UInt32(green) << 16) | (UInt32(blue) << 8) | UInt32(transparency)
+    public var value: UInt32 {
+        get { (UInt32(self._red) << 24) | (UInt32(self._green) << 16) | (UInt32(self._blue) << 8) | UInt32(self._alpha) }
+    }
+
+    init(_ red: UInt8, _ green: UInt8, _ blue: UInt8, alpha: UInt8 = 255) {
+        self._red = red
+        self._green = green
+        self._blue = blue
+    }
+
+    init(_ color: Color) {
+        self.init(UIColor(color))
+    }
+
+    init(_ color: UIColor) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        if color.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            self._red = UInt8(red * 255)
+            self._green = UInt8(green * 255)
+            self._blue = UInt8(blue * 255)
+        }
+        else {
+            self._red = 0
+            self._green = 0
+            self._blue = 0
+        }
+    }
+
+    init(_ value: UInt32) {
+        self._red = UInt8((value >> 24) & 0xFF)
+        self._green = UInt8((value >> 16) & 0xFF)
+        self._blue = UInt8((value >> 8) & 0xFF)
+        self._alpha = UInt8(value & 0xFF)
     }
 
     public var color: Color {
