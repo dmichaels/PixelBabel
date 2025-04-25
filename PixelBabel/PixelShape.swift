af98393651d004dada697647b2c9230e4297f81a
David Michaels <dmichaels@gmail.com>
2025-04-25 03:05:44 -0400

    etc

--- PixelBabel/PixelShape.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-enum PixelShape: String, CaseIterable, Identifiable {
-    case square = "Square"
-    case inset = "Inset"
-    case rounded = "Rounded"
-    case circle = "Circle"
-    var id: String { self.rawValue }
-}
