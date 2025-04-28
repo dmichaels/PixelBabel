a3812a8777da0199564fd3a350466248b53d11b3
David Michaels <dmichaels@gmail.com>
2025-04-28 09:36:01 -0400

    etc

---
+++ PixelBabel/LifeSettings.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+class DefaultLifeSettings
+{
+    public static let cellActiveColor: CellColor = CellColor(Color.green)
+    public static let cellInactiveColor: CellColor = CellColor(Color.red)
+}
