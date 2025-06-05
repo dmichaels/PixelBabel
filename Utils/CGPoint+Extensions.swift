30e3a9d247c27fefbeed875ea8bafefa6aee0973
David Michaels <dmichaels@gmail.com>
2025-06-04 22:40:47 -0400

    tec

--- Utils/CGPoint+Extensions.swift
+++
@@ -1,8 +0,0 @@
-import CoreGraphics
-import SwiftUI
-
-extension CGPoint: CustomStringConvertible {
-    public var description: String {
-        String(format: "[%.1f, %.1f]", self.x, self.y)
-    }
-}
